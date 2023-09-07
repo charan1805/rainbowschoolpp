43462992ead27e55d0e8f19a22575f898c6dadcd
charan1805 <sreecharanmudhiraj@gmail.com>
2023-09-07 11:49:04 +0530

    First class

---
+++ RainbowSchoolApi/RainbowSchoolApi/Migrations/20230907055504_SchoolMg.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RainbowSchoolApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchoolMg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
